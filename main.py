90fe6a0fa6209e6e3151b317b77b7c2f61d03903
Reymond Yncierto <rpyncierto@up.edu.ph>
2025-05-25 11:48:37 +0800

    chore: INTEGRATE conversion endpoint

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from routers.conversion_routes import router as conversion_router
 
 app = FastAPI()
+
+app.include_router(conversion_router, prefix="/convert", tags=["convert"])
 
 @app.get("/")
 def index():
